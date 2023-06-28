852e835ef58fb2a1488322c52305b38652e66e4e
Kowshiq Kattamuri <kattamuri_b201298bt@nitc.ac.in>
2023-06-28 07:41:31 +0530

    Create fourthquestion.cpp

---
+++ fourthquestion.cpp
@@ -0,0 +1,16 @@
+#include<bits/sdtc++.h>
+using namespace std;
+
+int searchInsert(vector<int>& nums, int target){
+    return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+}
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++){
+        cin >> nums[i];
+    }
+    cout << searchInsert(nums, m) << endl;
+}
