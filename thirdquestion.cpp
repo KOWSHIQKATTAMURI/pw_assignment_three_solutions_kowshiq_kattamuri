be196ab09d3b95fae8c22ebbe92608ff2672ebcd
Kowshiq Kattamuri <kattamuri_b201298bt@nitc.ac.in>
2023-06-28 07:40:11 +0530

    Create thirdquestion.cpp

---
+++ thirdquestion.cpp
@@ -0,0 +1,19 @@
+#include<bits/sdtc++.h>
+using namespace std;
+
+void nextPermutation(vector<int>& nums){
+    next_permutation(nums.begin(), nums.end());
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++){
+        cin >> nums[i];
+    }
+    nextPermutation(nums);
+    for(int i = 0; i < nums.size(); i++}{
+        cout << nums[i] << " ";
+    }
+}
