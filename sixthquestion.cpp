c580e170db9436d6f73afb93c46f2e1d6ad1942b
Kowshiq Kattamuri <kattamuri_b201298bt@nitc.ac.in>
2023-06-28 07:55:50 +0530

    Create sixthquestion.cpp

---
+++ sixthquestion.cpp
@@ -0,0 +1,19 @@
+#include<bits/sdtc++.h>
+using namespace std;
+
+int singleNumber(vector<int>& nums) {
+    auto kow = [](int& x, int& y){
+       return x^y;
+    };
+    return accumulate(nums.begin(), nums.end(), 0, kow);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++){
+        cin >> nums[i];
+    }
+    cout << singleNumber(nums) << endl;
+}
